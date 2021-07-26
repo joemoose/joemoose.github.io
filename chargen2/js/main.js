3e9b3588e1dc1d079418763c03904bc852fa8d73
Zachary Taylor <65364146+joemoose@users.noreply.github.com>
2021-07-26 16:10:05 -0500

    Update main.js

--- chargen2/js/main.js
+++ chargen2/js/main.js
@@ -1,16 +1,14 @@
-roll = (d) => {
+﻿roll = (d) => {
   return Math.floor(Math.random()*d)+1;
 };
 const armor = generate_text("armor");
 const helmet = generate_text("helmet");
 const tool = generate_text("tool");
 const gear = generate_text("gear");
-const trinket = generate_text("trinket");
 const weapons = generate_text("weapons");
-const bonus = generate_text("bonus");
 
 let allItems = [];
-allItems.push(armor, helmet, weapons, gear, tool, trinket, bonus);
+allItems.push(armor, helmet, weapons, gear, tool);
 let total = 2;
 for (let i = 0; i < allItems.length; i++) {
   let item = allItems[i];
@@ -40,7 +38,6 @@ armorValue.forEach((item) => {
 
 console.log(total);
 $("#character").html(generate_text("character"));
-$("#age").html(roll(20) + roll(20) + 10);
 $("#hp").html(roll(6));
 ["str", "dex", "wil"].forEach((item, i) => {
 $("#" + item).html(roll(6) + roll(6) + roll(6));});
@@ -49,8 +46,5 @@ $("#helmet").html(helmet);
 $("#weapons").html(weapons);
 $("#tool").html(tool);
 $("#gear").html(gear);
-$("#trinket").html(trinket);
-$("#bonus").html(bonus);
 $("#armorTotal").html(armorTotal);
-$("#total").html(total);
 $("#gold").html(roll(6) + roll(6) + roll(6));
