76a495cd58b03bd3f90b1ffc4035177c68c1d32e
Zachary Taylor <65364146+joemoose@users.noreply.github.com>
2021-07-26 16:18:53 -0500

    Update main.js

--- chargen2/js/main.js
+++ chargen2/js/main.js
@@ -5,10 +5,12 @@ const armor = generate_text("armor");
 const helmet = generate_text("helmet");
 const tool = generate_text("tool");
 const gear = generate_text("gear");
+const trinket = generate_text("trinket");
 const weapons = generate_text("weapons");
+const bonus = generate_text("bonus");
 
 let allItems = [];
-allItems.push(armor, helmet, weapons, gear, tool);
+allItems.push(armor, helmet, weapons, gear, tool, trinket, bonus);
 let total = 2;
 for (let i = 0; i < allItems.length; i++) {
   let item = allItems[i];
@@ -38,6 +40,7 @@ armorValue.forEach((item) => {
 
 console.log(total);
 $("#character").html(generate_text("character"));
+$("#age").html(roll(20) + roll(20) + 10);
 $("#hp").html(roll(6));
 ["str", "dex", "wil"].forEach((item, i) => {
 $("#" + item).html(roll(6) + roll(6) + roll(6));});
@@ -46,5 +49,8 @@ $("#helmet").html(helmet);
 $("#weapons").html(weapons);
 $("#tool").html(tool);
 $("#gear").html(gear);
+$("#trinket").html(trinket);
+$("#bonus").html(bonus);
 $("#armorTotal").html(armorTotal);
+$("#total").html(total);
 $("#gold").html(roll(6) + roll(6) + roll(6));
